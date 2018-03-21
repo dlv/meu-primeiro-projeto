438036e7d71152bf25eba36af84231644a6a4f21
Douglas Prado <douglas.prado@logicsp.com.br>
2018-03-21 13:23:45 -0300

    Integração com TravisCI

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+[![Build Status](https://travis-ci.org/dlv/meu-primeiro-projeto.svg?branch=master)](https://travis-ci.org/dlv/meu-primeiro-projeto)
+
 # meu-primeiro-projeto
 Projeto inicial Spring Boot
--- src/main/java/com/douglas/api/MeuPrimeiroProjetoApplication.java
+++ src/main/java/com/douglas/api/MeuPrimeiroProjetoApplication.java
@@ -8,5 +8,6 @@ public class MeuPrimeiroProjetoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
+		System.out.println("Meu primeiro projeto");
 	}
 }
