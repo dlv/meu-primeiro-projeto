2d7bfed430b37a23dec580f63bf64c6255b50931
Douglas Prado <douglas.prado@logicsp.com.br>
2018-05-07 15:27:40 -0300

    P14 - BCrypt

--- src/main/java/com/douglas/api/MeuPrimeiroProjetoApplication.java
+++ src/main/java/com/douglas/api/MeuPrimeiroProjetoApplication.java
@@ -25,37 +25,6 @@ public class MeuPrimeiroProjetoApplication {
 //		return args -> {
 //			System.out.println("\n");
 //			System.out.println("Meu primeiro projeto");
-//			System.out.println("### Quantidade de elementos por página = " + this.qtdPorPagina);
-//			String senhaEncoded = SenhaUtils.gerarBCrypt("123456");
-//			System.out.println("Senha encoded: " + senhaEncoded);	
-//			
-//			senhaEncoded = SenhaUtils.gerarBCrypt("123456");
-//			System.out.println("Senha encoded novamente: " + senhaEncoded);	
-//
-//			System.out.println("Senha válida: " + SenhaUtils.senhaValida("123456", senhaEncoded));
-//			System.out.println("\n");
-//			
-//			Empresa empresa = new Empresa();
-//			empresa.setRazaoSocial("Kazale IT");
-//			empresa.setCnpj("74645215000104");
-//			
-//			this.empresaRepository.save(empresa);
-//
-//			List<Empresa> empresas = empresaRepository.findAll();
-//			empresas.forEach(System.out::println);
-//			
-//			Empresa empresaDb = empresaRepository.findOne(1L);
-//			System.out.println("Empresa por ID: " + empresaDb);
-//			
-//			empresaDb.setRazaoSocial("Kazale IT Web");
-//			this.empresaRepository.save(empresaDb);
-//
-//			Empresa empresaCnpj = empresaRepository.findByCnpj("74645215000104");
-//			System.out.println("Empresa por CNPJ: " + empresaCnpj);
-//			
-//			this.empresaRepository.delete(1L);
-//			empresas = empresaRepository.findAll();
-//			System.out.println("Empresas: " + empresas.size());
 //			
 //			this.exemploService.testarServico();
 //		};
--- src/test/java/com/douglas/api/test/repositories/EmpresaTest.java
+++ src/test/java/com/douglas/api/test/repositories/EmpresaTest.java
@@ -1,4 +1,4 @@
-package com.douglas.api.test.repository;
+package com.douglas.api.test.repositories;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
