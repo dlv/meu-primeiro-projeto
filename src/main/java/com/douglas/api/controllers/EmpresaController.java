81f892d0ddb9d615b265c4e5ab9b5d771fead0e7
Douglas Prado <douglas.prado@logicsp.com.br>
2018-05-11 11:50:10 -0300

    P23 - Encapsulando o retorno de uma chamada a API Restfull (Response)

--- src/main/java/com/douglas/api/controllers/EmpresaController.java
+++ src/main/java/com/douglas/api/controllers/EmpresaController.java
@@ -7,14 +7,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.douglas.api.dtos.EmpresaDto;
+import com.douglas.api.responses.Response;
 
 @RestController
 @RequestMapping("/api/empresas")
 public class EmpresaController {
 
 	@PostMapping
-	public ResponseEntity<EmpresaDto> cadastrar(@RequestBody EmpresaDto empresaDto) {
+	public ResponseEntity<Response<EmpresaDto>> cadastrar(@RequestBody EmpresaDto empresaDto) {
+		Response<EmpresaDto> response = new Response<EmpresaDto>();
 		empresaDto.setId(1L);
-		return ResponseEntity.ok(empresaDto);
+		response.setData(empresaDto);
+		
+		return ResponseEntity.ok(response);
 	}
 }
