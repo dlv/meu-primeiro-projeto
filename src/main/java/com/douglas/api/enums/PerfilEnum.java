04b753b8914bc481f0b285851dc7b30c2e6b37aa
Douglas Prado <dc.prado@gmail.com>
2024-01-03 02:19:10 -0300

    Correção do projeto para o java 11

--- src/main/java/com/douglas/api/enums/PerfilEnum.java
+++
@@ -1,6 +0,0 @@
-package com.douglas.api.enums;
-
-public enum PerfilEnum {
-	ROLE_ADMIN,
-	ROLE_USUARIO;
-}
---
+++ src/main/java/com/kazale/api/MeuPrimeiroProjetoApplication.java
@@ -0,0 +1,13 @@
+package com.kazale.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuPrimeiroProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
+		System.out.println("Meu primeiro projeto...");
+	}
+}
